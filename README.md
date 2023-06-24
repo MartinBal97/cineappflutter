3f985f4f6eb12470185909c16d639dee93f381d6
martin-fusuma <127744754+martin-fusuma@users.noreply.github.com>
2023-06-24 12:31:31 +0200

    environment variable configure

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # cineappflutter
 
-# Dev
+## DEV
 
 1. Copiar el .env.template y renombrarlo a .env
 2. Cambiar las variables de entorno (The MovieDB)
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:cineappflutter/config/router/app_router.dart';
 import 'package:cineappflutter/config/theme/app_theme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MainApp());
 }
 
